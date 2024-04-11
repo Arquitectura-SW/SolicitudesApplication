0ac61304c865d2557de9c167d10bf28bb62b3578
Natalia Andrea Quiroga Castillo <69173681+nquirogac@users.noreply.github.com>
2024-04-10 21:30:51 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -5,7 +5,7 @@ import json
 
 from random import choice, randint, uniform
 
-rabbit_host = '10.128.0.5'
+rabbit_host = '10.128.0.53'
 rabbit_user = 'admin'
 rabbit_password = 'admin'
 exchange = 'BancoLosAlpes'
