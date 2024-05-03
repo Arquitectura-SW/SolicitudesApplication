1c3a17800a06f1f82d2e2430e9e5aa2cbd212265
jhsilvaz13 <jhsilvaz@unal.edu.co>
2024-05-03 10:53:46 -0500

    Error handling producer

--- producer.py
+++ producer.py
@@ -17,23 +17,26 @@ path.append('SolicitudesApplication/settings.py')
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'SolicitudesApplication.settings')
 django.setup()
 
-from solicitudes.logic.logic_solicitudes import createSolicitud, createSolicitudObject, getSolicitudes
 from solicitudes.models import Solicitud
 
 def brokerSol(solicitud:Solicitud):
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=rabbit_host, credentials=pika.PlainCredentials(rabbit_user, rabbit_password)))
-    channel = connection.channel()
-    channel.exchange_declare(exchange=exchange, exchange_type='topic')
+    try:
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=rabbit_host, credentials=pika.PlainCredentials(rabbit_user, rabbit_password)))
+        channel = connection.channel()
+        channel.exchange_declare(exchange=exchange, exchange_type='topic')
 
-    for topic in topics:
+        for topic in topics:
             print(solicitud.creationDate)
             payload = {'user_id': solicitud.user,'status':solicitud.status, 'creationDate': str(solicitud.creationDate)}
             message = json.dumps(payload)
             print("Topic: %r Status: %r, UserId: %r, CreationDate: %r" % (topic, solicitud.status, solicitud.user.document, solicitud.creationDate))
             time.sleep(1)
-    channel.basic_publish(exchange=exchange, routing_key=topic, body=message)
-    connection.close()
+            channel.basic_publish(exchange=exchange, routing_key=topic, body=message)
+        connection.close()
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Failed to connect to RabbitMQ: {e}")
+
             
         
         
