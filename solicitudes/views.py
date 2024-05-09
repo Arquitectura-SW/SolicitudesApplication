117bddf7a799ee7a4ead339dd46cd042189128bd
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-05-09 18:53:24 -0500

    Cambios views loginrequired

--- solicitudes/views.py
+++ solicitudes/views.py
@@ -6,7 +6,9 @@ from .serializer import SolicitudSerializer
 from producer import brokerSol
 from .logic.logic_solicitudes import getSolicitudes, createSolicitud, getSolicitudesByUserId, getSolicitudByStatus
 from SolicitudesApplication.auth0backend import getRole
+from django.contrib.auth.decorators import login_required
 
+@login_required
 @api_view(['GET'])
 def solicitudesList(request):
     if request.method == 'GET':
@@ -19,6 +21,7 @@ def solicitudesList(request):
             except Exception:
                 return Response({"error": "The Solicitudes wasn't found."}, status=status.HTTP_400_BAD_REQUEST)
 
+@login_required
 @api_view(['GET'])
 def solicitudesListByUserId(request, document):
     if request.method == 'GET':
@@ -31,6 +34,7 @@ def solicitudesListByUserId(request, document):
             except Exception:
                 return Response({"error": "The Solicitudes wasn't found."}, status=status.HTTP_400_BAD_REQUEST)
 
+@login_required
 @api_view(['GET'])
 def solicitudesListByStatus(request, status):
     if request.method == 'GET':
@@ -42,7 +46,8 @@ def solicitudesListByStatus(request, status):
                 return Response(serializer.data)
             except Exception:
                 return Response({"error": "The Solicitudes wasn't found."}, status=status.HTTP_400_BAD_REQUEST)
-                
+
+@login_required           
 @api_view(['POST'])
 def postSolicitud(request):
     if request.method == 'POST':
