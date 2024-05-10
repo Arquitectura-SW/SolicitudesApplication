a3e60181c2605214aa381611456e0bb72ea4d170
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-05-09 19:22:27 -0500

    cambios settings url login

--- SolicitudesApplication/settings.py
+++ SolicitudesApplication/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = "api/login/auth0" 
+LOGIN_URL = "/api/login/auth0" 
 LOGIN_REDIRECT_URL = "/api/solicitudes" 
 LOGOUT_REDIRECT_URL = "https://isis2503trodriten.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F104.198.253.65:8080" 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
