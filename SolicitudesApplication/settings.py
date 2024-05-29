f03a4c9acc07f3dc80626fe66e156711503c460b
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-05-29 15:25:39 -0500

    Cambios credenciales

--- SolicitudesApplication/settings.py
+++ SolicitudesApplication/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Sol',
-        'USER': 'admin',
+        'USER': 'monitoring_user',
         'PASSWORD': 'isis2503',
-        'HOST': '172.23.192.7',
+        'HOST': '10.63.112.3',
         'PORT': '',
     },
 }
