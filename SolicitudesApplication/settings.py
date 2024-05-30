14e8db84fc411df3e14ee2dceaef225dffedeb39
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-05-29 22:12:47 -0500

    Una instancia Sol Kong

--- SolicitudesApplication/settings.py
+++ SolicitudesApplication/settings.py
@@ -132,8 +132,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/login/auth0" 
-LOGIN_REDIRECT_URL = "/api/solicitudes" 
-LOGOUT_REDIRECT_URL = "https://isis2503trodriten.us.auth0.com/v2/logout?returnTo=https%3A%2F%2F34.107.242.117:80" 
+LOGIN_REDIRECT_URL = "" 
+LOGOUT_REDIRECT_URL = "https://isis2503trodriten.us.auth0.com/v2/logout?returnTo=https%3A%2F%2F34.29.48.100:8080" 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503trodriten.us.auth0.com' 
 SOCIAL_AUTH_AUTH0_KEY = 'qukDwvWR7N4OHN8T9KdcPXdTDUWRn83S' 
