a10995084640484920cd02d675c988da94cce220
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-05-29 22:41:05 -0500

    Debug Check again

--- solicitudes/logic/logic_solicitudes.py
+++ solicitudes/logic/logic_solicitudes.py
@@ -4,9 +4,13 @@ import requests
 from SolicitudesApplication import settings
 
 def check_user(data):
+    print(data)
     r = requests.get(settings.PATH_USERS, headers={"Accept":"application/json"})
+    print(r)
     users = r.json()
+    print(users)
     for user in users['data']:
+        print(user)
         if data["user"] == user["document"]:
             return True
     return False
